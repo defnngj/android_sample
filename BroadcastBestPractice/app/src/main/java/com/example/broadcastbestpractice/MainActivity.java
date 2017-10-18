2110449932a3ff12cd2efba4a0af619520228d4c
fnngj <fnngj@126.com>
2017-10-18 11:03:21 +0800

    add android broad sample

---
+++ BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.broadcastbestpractice;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button forceOffline =(Button) findViewById(R.id.force_offline);
+        forceOffline.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent("com.example.broadcastbestpractice.FORCE_OFFLINE");
+                sendBroadcast(intent);
+            }
+        });
+    }
+}
